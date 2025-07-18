3eaaf0e559352b6cdff561d3346daad03f35b866
mabel <sweetdreambb2022@gmail.com>
2025-07-18 12:05:20 +0800

    amend TransactionProductDataMapper to use Mapstruct

--- src/main/java/com/fsse2506/project/mapper/transactionProduct/TransactionProductDataMapper.java
+++ src/main/java/com/fsse2506/project/mapper/transactionProduct/TransactionProductDataMapper.java
@@ -3,32 +3,45 @@ package com.fsse2506.project.mapper.transactionProduct;
 import com.fsse2506.project.data.product.domainObject.response.ProductResponseData;
 import com.fsse2506.project.data.transactionProduct.domainObject.response.TransactionProductResponseData;
 import com.fsse2506.project.data.transactionProduct.entity.TransactionProductEntity;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-@Component
-public class TransactionProductDataMapper {
+@Mapper(componentModel="spring")
+public interface TransactionProductDataMapper {
 
-    public TransactionProductResponseData toTransactionProductResponseData(TransactionProductEntity transactionProductEntity,ProductResponseData productResponseData){
-        TransactionProductResponseData transactionProductResponseData=new TransactionProductResponseData();
-        transactionProductResponseData.setTpid(transactionProductEntity.getTpid());
-        transactionProductResponseData.setProductResponseData(productResponseData);
-        transactionProductResponseData.setQuantity(
-                transactionProductEntity.getQuantity()
-        );
-        transactionProductResponseData.setSubtotal(
-                productResponseData.getPrice().multiply(
-                        BigDecimal.valueOf(transactionProductEntity.getQuantity()
-                        )
-                )
-        );
-        return transactionProductResponseData;
+    @Mapping(target="tpid", source="transactionProductEntity.tpid")
+    @Mapping(target="productResponseData",source="productResponseData")
+    @Mapping(target="quantity",source="transactionProductEntity.quantity")
+    @Mapping(target="subtotal",expression="java(calculateSubtotal(productResponseData.getPrice(),transactionProductEntity.getQuantity()))")
+    TransactionProductResponseData toTransactionProductResponseData(
+            TransactionProductEntity transactionProductEntity,
+            ProductResponseData productResponseData
+    );
+
+    default BigDecimal calculateSubtotal(BigDecimal price, Integer quantity){
+        return price.multiply(BigDecimal.valueOf(quantity));
     }
-    public List<TransactionProductResponseData> toTransactionProductResponseDataList(List<TransactionProductEntity> transactionProductEntityList, List<ProductResponseData> productResponseDataList){
+//        transactionProductResponseData.setTpid(transactionProductEntity.getTpid());
+//        transactionProductResponseData.setProductResponseData(productResponseData);
+//        transactionProductResponseData.setQuantity(
+//                transactionProductEntity.getQuantity()
+//        );
+//        transactionProductResponseData.setSubtotal(
+//                productResponseData.getPrice().multiply(
+//                        BigDecimal.valueOf(transactionProductEntity.getQuantity()
+//                        )
+//                )
+//        );
+//
+    default List<TransactionProductResponseData> toTransactionProductResponseDataList(
+            List<TransactionProductEntity> transactionProductEntityList,
+            List<ProductResponseData> productResponseDataList
+    ){
         List<TransactionProductResponseData> transactionProductResponseDataList=new ArrayList<>();
-        for (int i=0; i<transactionProductEntityList.size(); i++){
+                for (int i=0; i<transactionProductEntityList.size(); i++){
             transactionProductResponseDataList.add(
                     toTransactionProductResponseData(
                             transactionProductEntityList.get(i),
@@ -38,5 +51,18 @@ public class TransactionProductDataMapper {
         }
         return transactionProductResponseDataList;
     }
+
+//    public List<TransactionProductResponseData> toTransactionProductResponseDataList(List<TransactionProductEntity> transactionProductEntityList, List<ProductResponseData> productResponseDataList){
+//        List<TransactionProductResponseData> transactionProductResponseDataList=new ArrayList<>();
+//        for (int i=0; i<transactionProductEntityList.size(); i++){
+//            transactionProductResponseDataList.add(
+//                    toTransactionProductResponseData(
+//                            transactionProductEntityList.get(i),
+//                            productResponseDataList.get(i)
+//                    )
+//            );
+//        }
+//        return transactionProductResponseDataList;
+//    }
 
 }
